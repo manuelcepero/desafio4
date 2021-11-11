4b4db85eb1cb4ca3e75a92a7cf643fb126522dee
Manuel Cepero Sánchez <manuel.cepero@hotmail.com>
2021-11-11 14:23:40 +0100

    Proyecto actualizado

--- src/main/java/com/example/desafio3/controlador/ClienteRestController.java
+++ src/main/java/com/example/desafio3/controlador/ClienteRestController.java
@@ -7,6 +7,7 @@ package com.example.desafio3.controlador;
 
 import com.example.desafio3.entidad.Cliente;
 import com.example.desafio3.servicio.ClienteServiceI;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,8 +28,8 @@ public class ClienteRestController {
     @Autowired
     private ClienteServiceI clienteService;
     
-    @GetMapping(value = "/{id}")
-    public Cliente getClientebyId(final @PathVariable Long id){
+    @GetMapping(value = "/clientes/{id}")
+    public Cliente obtenerClienteId(final @PathVariable Long id){
         Cliente c = clienteService.buscarCliente(id);
         return c;
     }
@@ -36,6 +37,17 @@ public class ClienteRestController {
     @PostMapping()
     public void anadirCliente(@RequestBody Cliente cliente){
         clienteService.guardar(cliente);
+    }
+    
+    @GetMapping(value = "/clientes")
+    public List<Cliente> obtenerClientes(){
+        return clienteService.obtenerClientes();
+    }
+    
+    @GetMapping(value = "/clientes/eliminar/{id}")
+    public void eliminarCliente(final @PathVariable Long id){
+        Cliente cliente = clienteService.buscarCliente(id);
+        clienteService.eliminar(cliente);
     }
     
 }
